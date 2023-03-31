e0a03eace5bf8c79692b16a8df5cded2a1b27320
Luisj5231 <59103217+Luisj5231@users.noreply.github.com>
2023-03-31 17:22:19 -0600

    Update output.tf
    output added

--- ec2-module/output.tf
+++ ec2-module/output.tf
@@ -7,3 +7,8 @@ output "ec2_instance_data" {
   value = aws_instance.ec2_instance
   description = "Specs of the primary network interface"
 }
+
+output "id_ec2_instance" {
+  value = aws_instance.ec2_instance.id
+  description = "Id of the created instance"
+}
