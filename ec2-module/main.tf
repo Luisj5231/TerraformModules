47da37e5733275d31d626e9c8cc5f963224b785c
Luisj5231 <59103217+Luisj5231@users.noreply.github.com>
2023-03-31 16:58:05 -0600

    Update main.tf
    fixed

--- ec2-module/main.tf
+++ ec2-module/main.tf
@@ -2,9 +2,10 @@ resource "aws_instance" "ec2_instance" {
   ami                         = var.amiId
   instance_type               = var.instanceType
   subnet_id                   = var.subnetId
-  key_name = var.EC2keyName
+  key_name                    = var.EC2keyName
   disable_api_termination     = false
   vpc_security_group_ids      = var.vpc_security_group_id
+  associate_public_ip_address = var.publicIPAddress
   
 
   
