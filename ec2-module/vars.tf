a1704423430ce4bbebdf9f1f26ed2ff48bfad0d3
Luisj5231 <59103217+Luisj5231@users.noreply.github.com>
2023-03-31 16:53:20 -0600

    uncommented line

--- ec2-module/vars.tf
+++ ec2-module/vars.tf
@@ -12,12 +12,12 @@ variable "subnetId" {
   type        = string
   description = "The VPC Subnet ID to launch in"
 }
-/*
+
 variable "vpc_security_group_id" {
   type        = list(any)
   description = "A list of security group IDs to associate with"
 }
-
+/*
 variable "publicIPAddress" {
   type        = bool
   description = "Set True for enabling Public IP Association"
